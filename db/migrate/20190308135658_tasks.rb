1dc53db3539c1bd4197e4765ab84264027b08826
DAN YAMAMOTO <danyamamoto@DANnoMacBook-ea.local>
2019-03-09 09:24:50 +0900

    test

--- db/migrate/20190308135658_tasks.rb
+++ db/migrate/20190308135658_tasks.rb
@@ -1,10 +1,16 @@
 class Tasks < ActiveRecord::Migration[5.2]
   def change
     create_table :Tasks do |t|
-      t.string :title
+      t.string :title :null => false
       t.time :estimated_time
-      t.time :spent_time
+      t.time :spent_time :default => 0
       t.time :excluded_time
+      t.text :description
+      t.text :comment
+      t.integer :user_id :null => false
+      t.integer :timer_status :null => false, :default => 0
+      t.timestamps :created_at
+      t.timestamps :last_modified_at
     end
   end
 end
--- db/migrate/20190308135711_users.rb
+++ db/migrate/20190308135711_users.rb
@@ -1,8 +1,9 @@
 class Users < ActiveRecord::Migration[5.2]
   def change
     create_table :Users do |t|
-      t.string :name
+      t.string :name :null => false
+      t.string :email :null => false
+      t.string :encrypted=password :null => false
     end
   end
 end
-ß
