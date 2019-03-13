aa470b52b0c76346179dabde0fc1602fb0f1f8d1
takenoko <tk.hrsk79@gmail.com>
2019-03-13 21:49:12 +0900

    create migration file and model, add rspec to gem

---
+++ db/migrate/20190313122936_create_tasks.rb
@@ -0,0 +1,16 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.integer :user_id, null: false
+      t.string :title, null: false
+      t.string :description
+      t.string :comment
+      t.time :estimated_time
+      t.time :spent_time, default: 0
+      t.time :exluded_time
+      t.integer :timer_status, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,28 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_03_13_122936) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.string "description"
+    t.string "comment"
+    t.time "estimated_time"
+    t.time "spent_time", default: "2000-01-01 00:00:00"
+    t.time "exluded_time"
+    t.integer "timer_status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
