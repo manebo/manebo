33fe80216a24f972c5fa8d6b9961112289e4aedf
takenoko <tk.hrsk79@gmail.com>
2019-03-23 09:25:51 +0900

    add model validators

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   validates :user_id, presence: true
-  validates :title, presence: true, length: {maximum: 20}
-  validates :description, presence: true, length: {maximum: 300}
-  validates :comment, presence: true, length: {maximum: 100}
+  validates :title, presence: true, length: {maximum: 50}
+  validates :description, presence: true, length: {maximum: 500}
+  validates :comment, presence: true, length: {maximum: 500}
   validates :timer_status, presence: true
 end
